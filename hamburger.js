6e0cf60a4f068e2e06e5053077958172da0cd078
soniyajacob31 <soniya.jacob06@gmail.com>
2025-11-24 23:33:51 -0600

    fixes + responsive

---
+++ hamburger.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+hamburger.addEventListener('click', () => {
+    mobileMenu.classList.toggle('show');
+});
